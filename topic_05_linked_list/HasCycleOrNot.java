a859fe213aa8f69f42aff4e53760bac62aa9cc57
khandakar Jarin Shova <jarinshova007@gmail.com>
2026-09-23 22:38:19 +0600

    modified

--- topic_05_linked_list/HasCycleOrNot.java
+++ topic_05_linked_list/HasCycleOrNot.java
@@ -15,9 +15,6 @@
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null) {
-            return false;
-        }
 
         ListNode fast = head;
         ListNode slow = head;
