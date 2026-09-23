35cb18062f0e14e719ab6eaef7ab7032c2f6306e
khandakar Jarin Shova <jarinshova007@gmail.com>
2026-09-23 17:03:55 +0600

    initial commit

---
+++ topic_05_linked_list/HasCycleOrNot.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
