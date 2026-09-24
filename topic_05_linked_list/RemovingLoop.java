bbe6f4d855dff97402e19944a7e8c97e84366ecb
khandakar Jarin Shova <jarinshova007@gmail.com>
2026-09-24 23:06:57 +0600

    initial commit

--- topic_05_linked_list/RemovingLoop.java
+++ topic_05_linked_list/RemovingLoop.java
@@ -1,9 +1,49 @@
 package topic_05_linked_list;
-// Removing Loops in a Linked List
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+} */
 
-// Time complexity - O(n) ????
-// Space complexity - O(1) ????
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
-public class RemovingLoop {
+class Solution {
+  public static void removeLoop(Node head) {
+    // code here
+    Node slow = head;
+    Node fast = head;
 
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+
+      if (slow == fast) {
+
+        slow = head;
+
+        // if cycle starts from head
+        if (slow == fast) {
+          while (fast.next != slow) {
+            fast = fast.next;
+          }
+
+          fast.next = null;
+          return;
+        }
+
+        while (slow.next != fast.next) {
+          slow = slow.next;
+          fast = fast.next;
+        }
+
+        fast.next = null;
+        return;
+      }
+    }
+  }
 }
